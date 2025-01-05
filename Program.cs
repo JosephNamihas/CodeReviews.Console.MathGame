f109b22d03620cfc89c6f63ed20a13d748f16c44
Joseph Namihas <joenamihas@yahoo.co.uk>
2025-01-05 20:21:41 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random rnd = new Random();
+int numberOne = 0;
+int numberTwo = 0;
+int score = 0;
+List <string> questionList = new List <string>();
+string question = "";
+double answer = 0;
+
+string? operationChoice;
+
+GameFlow();
+
+void GameFlow() {
+    StartQuiz();
+    GenerateQuestion(operationChoice);
+
+    Console.WriteLine($"{CompareAnswers(answer)}! Your score is {score}");
+
+    ViewQuestionList();
+}
+
+bool IsValidOperation(string? choice)
+{
+    if (string.IsNullOrWhiteSpace(choice))
+    {
+        Console.WriteLine("You need to enter a mathematical operation. It can't be empty!");
+        return false; // Checks for whitespace / empty strings
+    } 
+    
+    else if (choice != "+" && choice != "-" && choice != "*" && choice != "/") // If choice isn't any of the mathematical operators
+    {
+        Console.WriteLine("You need to enter a valid operator (+, -, /, *).");
+        return false; // Checks for correct characters
+    } 
+
+    else {
+        return true;
+    }
+}
+
+string StartQuiz() {+
+    do
+{
+    Console.WriteLine("Enter a mathematical operation (+, -, *, /):");
+    operationChoice = Console.ReadLine();
+
+} while (!IsValidOperation(operationChoice));
+
+return operationChoice; // while isValidOperation return false, keep running isValidOperation with the operation choice parameter. 
+
+}
+
+string GenerateQuestion(string operation) {
+
+    switch (operation)
+    {
+        case "+":
+            numberOne = rnd.Next(0, 99);
+            numberTwo = rnd.Next(0, 99);
+            Console.WriteLine("You chose addition.");
+            Console.WriteLine($"What is {numberOne} + {numberTwo}");
+            question = $"What is {numberOne} + {numberTwo}?";
+            answer = numberOne + numberTwo;
+
+            return question;
+
+        case "-":
+            numberOne = rnd.Next(0, 99);
+            numberTwo = rnd.Next(0, 99);
+            Console.WriteLine("You chose subtraction.");
+            Console.WriteLine($"What is {numberOne} - {numberTwo}?");
+            question = $"What is {numberOne} - {numberTwo}?";
+            answer = numberOne - numberTwo;
+            return question;
+
+        case "*":
+            numberOne = rnd.Next(0, 12);
+            numberTwo = rnd.Next(0, 12);
+            Console.WriteLine("You chose multiplication.");
+            Console.WriteLine($"What is {numberOne} * {numberTwo}?");
+            question = $"What is {numberOne} * {numberTwo}?";
+            answer = numberOne * numberTwo;
+            return question;
+
+        case "/":
+        Console.WriteLine("You chose division");
+            return DivisionLogic(numberOne, numberTwo);
+
+        default:
+            Console.WriteLine("Unexpected operation.");
+            return "Please restart quiz";
+    }
+}
+
+bool CompareAnswers(double answer) {
+    string? answerPrompt = Console.ReadLine();
+    double playerAnswer = double.Parse(answerPrompt);
+
+    if(answer == playerAnswer) {
+        score += 1;
+        questionList.Add(question);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void ViewQuestionList() {
+    Console.WriteLine("Press q to view the question list, otherwise any other key to continue");
+    Console.WriteLine("Press e to exit the application");
+    string? viewQuestions = Console.ReadLine();
+
+    /*if(string.IsNullOrWhiteSpace(viewQuestions)) {
+    Console.WriteLine("Press q to view the question list, otherwise any other key to continue");
+    }*/
+
+    viewQuestions.ToLower();
+
+    // if viewQuestions == e, exit game
+
+    if (viewQuestions == "q") {
+        QuestionList();
+        Console.WriteLine("Press any key to go to the next question");
+        Console.ReadKey();
+        GameFlow();
+    } else {
+        GameFlow();
+    }
+}
+
+void QuestionList() {
+
+    Console.WriteLine("Previous Questions: ");
+
+    foreach(string item in questionList) {
+        Console.WriteLine($"{item}\n");
+    }
+}
+
+string DivisionLogic(int numberOne, int numberTwo) {
+
+    do {
+        numberOne = rnd.Next(1, 12);
+        numberTwo = rnd.Next(1, 12);
+
+    } while (numberOne % numberTwo != 0); 
+
+        Console.WriteLine($"What is {numberOne} / {numberTwo}?");
+        question = $"What is {numberOne} / {numberTwo}?";
+        answer = numberOne / numberTwo;
+        return question;
+    }
+
+
---
+++ README.md
@@ -0,0 +1,33 @@
+# Math Quiz Project
+
+## Requirements
+<br>
+
+- You need to create a Math game containing the 4 basic operations .<strong>COMPLETE</strong>
+
+- The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100. Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer. <strong>COMPLETE</strong>
+
+- Users should be presented with a menu to choose an operation <strong>COMPLETE</strong>
+
+- You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games. <strong>COMPLETE</strong>
+
+- You don't need to record results on a database. Once the program is closed the results will be deleted. <strong>COMPLETE</strong>
+
+<br>
+
+## Future Challenges
+
+- Add a Random Game Option
+
+- Add a Timer 
+
+- Combine the two to create an 'Arcade Mode'
+
+- Add Difficulty Options
+
+## Lessons Learnt
+<br>
+
+<p> When to use methods. Can you overdo it with methods? Generally, the DRY principle is followed, by I felt like I made the code more complicated and harder to read by adding additional methods. My switch-case statement was originally bloated from methods (validation mainly). I took them out an decided to use the switch-case to hold more of the logic
+
+<p> I also went ahead and added the random game option. The decision making possibilites of the program then spiralled a bit out of control and I felt it was a bit messy. Highlighted the importance of modular code that can scale up.+
